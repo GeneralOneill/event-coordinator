684ae8095c72961f30a415d26a355e665d44ce4c
CSSI 2015 <CSSI@cssis-air.attlocal.net>
2015-08-03 21:03:42 -0500

    add number_of_people as part of the filter.

--- event-coordinator-15-08/main.py
+++ event-coordinator-15-08/main.py
@@ -19,7 +19,6 @@ import jinja2
 import os
 from google.appengine.ext import ndb
 
-
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -27,7 +26,28 @@ class Place(ndb.Model):
     name = ndb.StringProperty(required = True)
     location = ndb.StringProperty(required = True)
     value = ndb.StringProperty(required = True)
-    category = ndb.StringProperty()
+    category = ndb.StringProperty(repeated = True)
+
+portillos = Place(name = "Portillos", location = "100 W Ontario St, Chicago, IL 60654", value = "4", category = ["Food"])
+giordanos = Place(name = "Giordanos", location = "700 E Grand Ave, Chicago, IL 60611", value = "2", category = ["Food"])
+
+Sports_1 = Place(name = "Football", location = "1", value = "1", category = ["Sports"])
+Sports_2 = Place(name = "Soccer", location = "1", value = "3", category = ["Sports"])
+
+Entertainment_1 = Place(name = "Movies", location = "1", value = "1", category = ["Entertainment"])
+Entertainment_2 = Place(name = "Shows", location = "1", value = "2", category = ["Entertainment"])
+
+Recreation_1 = Place(name = "Happy Times", location = "1", value = "4", category = ["Recreation"])
+Recreation_2 = Place(name = "Fun Times", location = "1", value = "2", category = ["Recreation"])
+
+# portillos.put()
+# giordanos.put()
+# Sports_1.put()
+# Sports_2.put()
+# Entertainment_1.put()
+# Entertainment_2.put()
+# Recreation_1.put()
+# Recreation_2.put()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -35,21 +55,21 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write(template.render())
         self.response.out.write('Click here to get your results!')
 
-
 class SelectionHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/selections.html')
 
-        response = self.request.get('interest')
-        self.response.write(response)
+        interest = self.request.get('interest')
+        self.response.write(interest)
 
-        results = Place.query(Place.category == response).fetch()
+        number_of_people = self.request.get('number_of_people')
+        self.response.write(number_of_people)
+
+        results = Place.query(Place.category == interest, Place.value == number_of_people).fetch()
         template_vars = {"results": results}
 
         self.response.out.write(template.render(template_vars))
         self.response.out.write('Here are your results!')
-
-
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
